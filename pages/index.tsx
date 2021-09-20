396f60499de17c31b56a013225b352196d10b88b
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-09-20 10:16:26 +0400

    feat: add Example component and rename Home to IndexPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,20 @@
+/* Vendor */
 import Head from "next/head";
 
-const Home = () => (
-  <div className="container">
+/* Components */
+import { Example } from "@/components/Example/Example";
+
+const IndexPage = () => (
+  <>
     <Head>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-      <title>Title</title>
-
-      <meta name="description" content="Description" />
-      <meta property="og:title" content="Title" />
-      <meta property="og:description" content="Description" />
-      <meta property="og:image" content="https://www.mywebsite.com/image.jpg" />
-      <meta property="og:image:alt" content="Image alt" />
-      <meta property="og:locale" content="ru_RU" />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="og:url" content="" />
-
-      <link rel="icon" href="/favicon.ico" />
-      {/* <!-- 32×32 -->*/}
-      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-      {/*<!-- 512×512 -->*/}
-      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      {/*<!-- 180×180 -->*/}
-      <link rel="manifest" href="/manifest.webmanifest" />
-      <meta name="theme-color" content="#FF00FF" />
+      <title>Main page title, if you want to set title or other meta tags</title>
     </Head>
     <main>
-      <h1>Hello, my friend!</h1>
+      <Example>
+        <h1>Hello, my friend!</h1>
+      </Example>
     </main>
-  </div>
+  </>
 );
 
-export default Home;
+export default IndexPage;
