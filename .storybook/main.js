e5d0bedd68783a89aadd3ca01be8322cde53f7ba
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-10-20 15:42:34 +0400

    feat: add TsconfigPathsPlugin plugin and storybook-css-modules-preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,16 @@
+const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "storybook-css-modules-preset"],
+  webpackFinal: (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+        baseUrl: ".",
+      }),
+    ];
+    return config;
+  },
 };
